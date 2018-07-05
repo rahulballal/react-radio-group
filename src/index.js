89c7d716644f582440d6d0f33e37d7b626f39739
Rahul Ballal <rahulballal@gmail.com>
2018-07-05 22:49:19 +1000

    feat: integrate recompose

--- src/index.js
+++ src/index.js
@@ -1,40 +1,33 @@
-import React from 'react'
 import PropTypes from 'prop-types'
+import { compose, withState, withHandlers, lifecycle } from 'recompose'
 import RadioGroup from './components/radio-group'
 
-export default class extends React.PureComponent {
-  state = { selectedValue: this.props.selectedValue }
+export const handlers = {
+  onSelection: props => newSelection => props.setSelectedValue(newSelection)
+}
 
-  static propTypes = {
-    onSelectionChange: PropTypes.func.isRequired,
-    groupName: PropTypes.string.isRequired,
-    items: PropTypes.arrayOf(
-      PropTypes.shape({
-        label: PropTypes.string.isRequired,
-        value: PropTypes.string.isRequired
-      })
-    ),
-    selectedValue: PropTypes.string.isRequired
-  }
-
-  handleSelection = newSelection =>
-    this.setState(() => ({ selectedValue: newSelection }))
-
+export const lifecycleMethods = {
   componentDidUpdate () {
-    this.props.onSelectionChange(this.state.selectedValue)
-  }
-
-  render () {
-    const { groupName, items } = this.props
-    const { selectedValue } = this.state
-
-    return (
-      <RadioGroup
-        groupName={groupName}
-        items={items}
-        selectedValue={selectedValue}
-        onSelection={this.handleSelection}
-      />
-    )
+    this.props.onSelectionChange(this.props.selectedValue)
   }
 }
+
+const recomposed = compose(
+  withState('selectedValue', 'setSelectedValue', ''),
+  withHandlers(handlers),
+  lifecycle(lifecycleMethods)
+)(RadioGroup)
+
+recomposed.propTypes = {
+  onSelectionChange: PropTypes.func.isRequired,
+  groupName: PropTypes.string.isRequired,
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired
+    })
+  ),
+  selectedValue: PropTypes.string.isRequired
+}
+
+export default recomposed
